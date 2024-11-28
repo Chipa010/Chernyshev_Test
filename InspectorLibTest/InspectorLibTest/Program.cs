120be6210ecb04c08757711e42d39188bfd7dc6f
Chipa010 <porovozova14@gmail.com>
2024-11-28 19:45:57 +0900

    CommitTest

---
+++ InspectorLibTest/InspectorLibTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using InspectorLib;
+
+namespace InspectorLibTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionInsp inspector = new FunctionInsp();
+
+            
+            Console.WriteLine("Главный инспектор: " + inspector.GetInspector());
+            Console.WriteLine("Название автоинспекции: " + inspector.GetCarInspection());
+
+            if (inspector.SetInspector("Иванов И.И."))
+            {
+                Console.WriteLine("Главный инспектор успешно изменен на: " + inspector.GetInspector());
+            }
+            else
+            {
+                Console.WriteLine("Не удалось изменить главного инспектора.");
+            }
+
+           
+            string generatedNumber = inspector.GenerateNumber("1234", "A", "75");
+            Console.WriteLine("Сгенерированный госномер: " + generatedNumber);
+
+            Console.WriteLine("Сотрудники автоинспекции:");
+            foreach (var worker in inspector.GetWorker())
+            {
+                Console.WriteLine(worker);
+            }
+
+            inspector.AddWorker("Петров П.П.");
+            Console.WriteLine("После добавления нового сотрудника:");
+            foreach (var worker in inspector.GetWorker())
+            {
+                Console.WriteLine(worker);
+            }
+        }
+    }
+}
